5e378e13503eb091b619cc1eea18159c4a104d26
ubitinaitis <bitinaitis.u@northeastern.edu>
2022-02-12 01:03:11 -0500

    Update Goose.vue

--- src/components/Goose.vue
+++ src/components/Goose.vue
@@ -1,13 +1,14 @@
 <template>
     <div style=" height: 100px; display: flex; justify-content: center; align-items: center;`">
         <img alt="Goose image" src="../assets/goose.png" style="height: 100%; display: inline-block;" /> 
-        <h3> {{ name }} </h3>
+        <h3> {{ name }} </h3> <!-- the {{}} mean it is code, so it's plugging in the value of name --> 
     </div>
 </template>
 
 <script lang="ts">
 export default {
     name: 'Goose',
-    props: ['name']
+    props: ['name']  // the brackets mean it's a list
+    // a prop is a parameter that a component takes in
 }
 </script>
