7d3f02650339aa1ed84a2cf581c2ef7dff6d06ab
rawsodium <deannat2003@gmail.com>
2022-02-12 05:29:15 -0500

    info tab

---
+++ src/views/Info.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="info">
+    <h1> Info </h1>
+    <p> You are given your first goose Huey by Mrs. Fens!</p>
+    <p> Hooray!</p>
+    <p> She wants you to take care of Huey. </p>
+  </div>
+</template>
