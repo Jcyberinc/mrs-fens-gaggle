2149e2aa2e3c6c3d0f100d89ea15ab405bf388c2
rawsodium <deannat2003@gmail.com>
2022-02-12 05:48:45 -0500

    changed text in info tab

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="info">
-    <h1 class="strokeme"> Info </h1>
+    <h1 class = "strokeme"> Info </h1>
     <p> You are given your first goose Huey by Mrs. Fens!</p>
     <p> Hooray!</p>
     <p> She wants you to take care of Huey. </p>
+    <p> Breed your geese to get the best traits and bring them to the contest! </p>
+    <p> Mrs. Fens will be the judge </p>
+    <p> May the best geese win :) </p>
   </div>
 </template>
