1650d8793a7e01aacb0bf8d2a030d798ff394882
Jcyberinc <varga.j@northeastern.edu>
2022-02-11 23:39:58 -0500

    Update Goose

--- src/components/Goose.vue
+++ src/components/Goose.vue
@@ -2,6 +2,8 @@
     <div style="height: 100px; display: flex; justify-content: center; align-items: center;">
         <img alt="Goose image" src="../assets/goose.png" style="transform: scale(0.5);" /> 
         <h3> {{ name }} </h3>
+        <h5> Honk </h5>
+        <b-progress :value=honk :max=100 show-value class="mb-3"></b-progress>
     </div>
 </template>
 
