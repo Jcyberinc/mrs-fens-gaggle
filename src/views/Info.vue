12db2365ade76218ec1892ccd4b11d4d664ba06e
rawsodium <deannat2003@gmail.com>
2022-02-12 18:10:29 -0500

    changed instructions in info tab

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -4,8 +4,8 @@
     <p> You are given your first goose Huey by Mrs. Fens!</p>
     <p> Hooray!</p>
     <p> She wants you to take care of Huey. </p>
-    <p> Breed your geese to get the best traits and bring them to the contest! </p>
-    <p> Mrs. Fens will be the judge </p>
+    <p> Breed the geese you have at your disposal, as well as any who happen to wander in, by clicking on the two you wish to breed. </p>
+    <p> If you don’t take care of them well enough, they’ll run off earlier to find a better caretaker. </p>
     <p> May the best geese win :) </p>
   </div>
 </template>
