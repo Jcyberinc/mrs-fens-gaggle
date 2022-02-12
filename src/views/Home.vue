c5ad807a991eff1810804797b09e4275215d6bcb
Derek Kaplan <thederekkaplan@protonmail.com>
2022-02-11 22:20:28 -0500

    Resolve merge conflicts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="home">
     <h1>Mrs. Fens' Gaggle</h1>
-    <Goose name="Huey" />
+    <GooseComponent v-for="goose in geese" :key="goose.name" :name="goose.name" style="padding:10px" />
   </div>
 </template>
 
-<script>
-import Goose from '@/components/Goose.vue'
+<script lang="ts">
+import GooseComponent from '@/components/Goose.vue';
+
+class Goose {
+  name: string
+  constructor(name: string) {
+    this.name = name
+  }
+}
 
 export default {
   name: 'Home',
   components: {
-    Goose
+    GooseComponent
+  },
+  data: function() {
+    return {
+      geese: [
+        new Goose('Huey'),
+        new Goose('Dewey'),
+        new Goose('Louie')
+      ]
+    }
   }
 }
 </script>
