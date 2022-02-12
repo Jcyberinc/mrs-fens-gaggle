ca0d8011167e1cd957cae038b79364697f09417f
rawsodium <deannat2003@gmail.com>
2022-02-12 01:14:09 -0500

    table without bar

--- src/components/Goose.vue
+++ src/components/Goose.vue
@@ -12,17 +12,14 @@
         <tr>
         <td>&nbsp;Honk
 >>>>>>> Stashed changes
-            <b-progress :value="honk" :max=100 show-progress animated></b-progress>
         </td>
         </tr>
         <tr>
         <td>&nbsp;Wingspan
-            <b-progress :value="wingspan" :max=100 show-progress animated></b-progress>
         </td>
         </tr>
         <tr>
         <td>&nbsp;Neck Size
-            <b-progress :value="neck" :max=100 show-progress animated></b-progress>
         </td>
         </tr>
         </tbody>
