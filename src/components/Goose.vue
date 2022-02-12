01bce10d67f581ea6534b5d2921ae52a43829eea
rawsodium <deannat2003@gmail.com>
2022-02-12 01:18:09 -0500

    table update

--- src/components/Goose.vue
+++ src/components/Goose.vue
@@ -1,17 +1,11 @@
 <template>
     <div style="height: 100px; display: flex; justify-content: center; align-items: center;">
         <img alt="Goose image" src="../assets/goose.png" style="transform: scale(0.5);" /> 
-<<<<<<< Updated upstream
-        <h3> {{ name }} </h3> <!-- the {{}} mean it is code, so it's plugging in the value of name -->
-        <div>
-            <h5> Honk </h5>
-=======
         <h3> {{ name }} </h3>
         <table>
         <tbody>
         <tr>
         <td>&nbsp;Honk
->>>>>>> Stashed changes
         </td>
         </tr>
         <tr>
@@ -23,6 +17,7 @@
         </td>
         </tr>
         </tbody>
+        </table>
     </div>    
 </template>
 
