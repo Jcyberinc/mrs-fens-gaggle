f4ab981aaca969a8aef4cd74621e0420f7722e3e
rawsodium <deannat2003@gmail.com>
2022-02-11 20:57:21 -0500

    your mother
    lmao

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <p> Sussy Amongus </p>
+    <p> Your mother and me </p>
     <HelloWorld msg="Mrs. Fens' Gaggle"/>
   </div>
 </template>
