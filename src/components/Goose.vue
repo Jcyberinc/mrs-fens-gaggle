de5391390ad55e7becd2cc518f3de2d0069965c1
Jcyberinc <varga.j@northeastern.edu>
2022-02-12 05:29:55 -0500

    Changed Cute/Cool Math

--- src/components/Goose.vue
+++ src/components/Goose.vue
@@ -1,62 +1,93 @@
 <template>
-    <div style="height: 100px; display: flex; justify-content: center; align-items: center;">
-        <span
-        @mouseenter="hover = true"
-        @mouseleave="hover = false">
-        <h3> {{cuteString()}} {{ name }} </h3>
-        <img alt="Goose image" :src="sprite" style="transform: scale(0.5);" />
-        </span>
-        <span v-if="hover">
-        <table>
+  <div
+    style="
+      height: 100px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    "
+  >
+    <span @mouseenter="hover = true" @mouseleave="hover = false">
+      <h3>{{ status() }} {{ name }}</h3>
+      <img alt="Goose image" :src="sprite" style="transform: scale(0.5)" />
+    </span>
+    <span v-if="hover">
+      <table>
         <tbody>
-        <tr>
-        <td>&nbsp;Honk
-            <b-progress :value="honk" :max=100 variant="info" striped animated></b-progress>
-        </td>
-        </tr>
-        <tr>
-        <td>&nbsp;Wingspan
-            <b-progress :value="wingspan" :max=100 variant="info" striped animated></b-progress>
-        </td>
-        </tr>
-        <tr>
-        <td>&nbsp;Neck Size
-            <b-progress :value="neck" :max=100 variant="info" striped animated></b-progress>
-        </td>
-        </tr>
+          <tr>
+            <td>
+              &nbsp;Honk
+              <b-progress
+                :value="honk"
+                :max="100"
+                variant="info"
+                striped
+                animated
+              ></b-progress>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              &nbsp;Wingspan
+              <b-progress
+                :value="wingspan"
+                :max="100"
+                variant="info"
+                striped
+                animated
+              ></b-progress>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              &nbsp;Neck Size
+              <b-progress
+                :value="neck"
+                :max="100"
+                variant="info"
+                striped
+                animated
+              ></b-progress>
+            </td>
+          </tr>
         </tbody>
-        </table>
-        </span>
-    </div>
+      </table>
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
-
 export default {
-      data() {
+  data() {
     return {
       hover: false,
-      isCute: (this.cute > this.cool)
     };
   },
   methods: {
-    cuteString() {
-        if(this.isCute) {
-            return "Cute"
+    status() {
+      if (this.cute > 200 && this.cool > 200) {
+        if (this.cute > this.cool) {
+          return "Cute";
         } else {
-            return "Cool"
+          return "Cool";
         }
-    }
+      } else if (this.cute > 200 && this.cool < 200) {
+        return "Cute";
+      } else if (this.cute < 200 && this.cool > 200) {
+        return "Cool";
+      } else {
+        return "";
+      }
+    },
   },
-props: {
+  props: {
     name: String,
     honk: Number,
     wingspan: Number,
     neck: Number,
-        cute: Number,
+    cute: Number,
     cool: Number,
-    sprite: String
-  }
-}
-
+    sprite: String,
+  },
+};
 </script>
