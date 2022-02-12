7f6ad4ce274e7c6e30676ef9724cd1b6bad868de
Jcyberinc <varga.j@northeastern.edu>
2022-02-12 01:39:34 -0500

    Update About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
-    <h1>What is this game?!?!?!</h1>
-    <p> Idk lol </p>
+    <h1>Mrs. Fens' Gaggle Production Staff</h1>
+    <p>Project Lead: Derek Kaplan</p>
+    <p>Lead Graphic Designer: Ula Bitinaitas</p>
+    <p>Lead Composer: Deanna Turner</p>
+    <p>Lead Script Writer: Emily Xu</p>
+    <p>Code Monkeys: Luis Toral and Julian Varga</p>
+    <p>Non-Lead Composer: Julian Varga</p>
+    <p>Moral Support: Caitlin Shen</p>
   </div>
 </template>
