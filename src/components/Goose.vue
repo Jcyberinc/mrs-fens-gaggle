2d79e618cfc71320659c2984dca461e051718d75
rawsodium <deannat2003@gmail.com>
2022-02-12 00:53:42 -0500

    line break

--- src/components/Goose.vue
+++ src/components/Goose.vue
@@ -2,23 +2,21 @@
     <div style="height: 100px; display: flex; justify-content: center; align-items: center;">
         <img alt="Goose image" src="../assets/goose.png" style="transform: scale(0.5);" /> 
         <h3> {{ name }} </h3>
-    </div>
-    <div> 
         <div>
             <h5> Honk </h5>
             <b-progress :value="honk" :max=100 show-progress animated></b-progress>
         </div>
-        &nbsp;
+        <br />
         <div>
             <h5> Wingspan </h5>
             <b-progress :value="wingspan" :max=100 show-progress animated></b-progress>
         </div>
-        &nbsp;
+        <br />
         <div>
             <h5> Neck Size </h5>
             <b-progress :value="neck" :max=100 show-progress animated></b-progress>
         </div>
-    <div>    
+    </div>    
 </template>
 
 <script lang="ts">
