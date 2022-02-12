3ad3e5ae9838a4ae2eb711ae1667c6ce7efe18be
Jcyberinc <varga.j@northeastern.edu>
2022-02-12 02:48:43 -0500

    Hat Fix/CoolCute Fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <img alt="Mrs. Fens' Gaggle" src="../assets/logo.gif" style="height: 100%; display: inline-block; margin-bottom: 30px" /> 
     <main style="display: flex; flex-flow: row wrap; justify-content: center; gap: 20px;">
       <GooseComponent v-for="goose in geese" :key="goose.name" :name="goose.name" :honk="goose.honk"
-      :wingspan="goose.wingspan" :neck="goose.neck" :sprite="goose.sprite"/>
+      :wingspan="goose.wingspan" :neck="goose.neck" :cute="goose.cute" :cool="goose.cool" :sprite="goose.sprite"/>
     </main>
   </div>
 </template>
@@ -16,12 +16,16 @@ class Goose {
   honk: number
   wingspan: number
   neck: number
+  cute: number
+  cool: number
   sprite: string
-  constructor(name: string, honk: number, wingspan: number, neck: number, sprite: string) {
+  constructor(name: string, honk: number, wingspan: number, neck: number, cute: number, cool: number, sprite: string) {
     this.name = name
     this.honk = honk
     this.wingspan = wingspan
     this.neck = neck
+    this.cute = cute
+    this.cool = cool
     this.sprite = sprite
   }
 }
@@ -31,6 +35,8 @@ class Goose {
     childGooseHelperHonk(firstGoose, secondGoose),
     childGooseHelperWingspan(firstGoose, secondGoose),
     childGooseHelperNeck(firstGoose, secondGoose),
+    randomNum(), //PLACEHOLDER CODE UNTIL ACTUAL HELPERS MADE
+    randomNum(), //PLACEHOLDER CODE UNTIL ACTUAL HELPERS MADE
     sprite)
   }  
 
@@ -56,7 +62,7 @@ function childGooseHelperName(firstGoose: Goose, secondGoose: Goose): string {
 
 // random stat generator for the spawning geese
 function randomStatGen(): Goose {
-  return new Goose("StubNameGen", randomNum(), randomNum(), randomNum(), "no hat" )
+  return new Goose("StubNameGen", randomNum(), randomNum(), randomNum(), randomNum(), randomNum(), require("@/assets/goosefinal.png"))
 }
 
 // creates the random number 
@@ -81,7 +87,7 @@ class HatType {
     this.cute = cute
     this.cool = cool
   }
-}
+} 
 
 export default {
   name: 'Home',
@@ -91,20 +97,20 @@ export default {
   data: function() {
     return {
       geese: [
-        new Goose('Huey', 50, 30, 10, require("@/assets/goosefinal.png"))
+        new Goose('Huey', 50, 30, 10, 0, 0, require("@/assets/goosefinal.png"))
       ],
       hats: [
-        new HatType('No Hat', "/@/assets/goosefinal.png", 0, 0),
-        new HatType('Propeller', "/@/assets/goosepropellor.png", 7, -2),
-        new HatType('Top Hat', "/@/assets/goosetophat.png", -3, 8),
-        new HatType('Bearskin', "/@/assets/goosebearskin.png", -5, 10),
-        new HatType('Party Hat', "/@/assets/partygoose.png", 6, -1),
-        new HatType('Crown', "/@/assets/kinggoose.png", -2, 7),
-        new HatType('Bonnet', "/@/assets/babygoose.png", 9, -4),
-        new HatType('Santa', "/@/assets/santagoose.png", 6, -1),
-        new HatType('Chefs Hat', "/@/assets/chefgoose.png", 1, 4),
-        new HatType('Frog Hat', "/@/assets/chefgoose.png", 10, -5),
-        new HatType('Rotten Banana', "/@/assets/bananagoose.png", -10, -10)
+        new HatType('No Hat', require("@/assets/goosefinal.png"), 0, 0),
+        new HatType('Propeller', require("@/assets/goosepropellor.png"), 7, -2),
+        new HatType('Top Hat', require("@/assets/goosetophat.png"), -3, 8),
+        new HatType('Bearskin', require("@/assets/goosebearskin.png"), -5, 10),
+        new HatType('Party Hat', require("@/assets/partygoose.png"), 6, -1),
+        new HatType('Crown', require("@/assets/kinggoose.png"), -2, 7),
+        new HatType('Bonnet', require("@/assets/babygoose.png"), 9, -4),
+        new HatType('Santa', require("@/assets/santagoose.png"), 6, -1),
+        new HatType('Chefs Hat', require("@/assets/chefgoose.png"), 1, 4),
+        new HatType('Frog Hat', require("@/assets/chefgoose.png"), 10, -5),
+        new HatType('Rotten Banana', require("@/assets/banangoose.png"), -10, -10)
       ]
     }
   }
