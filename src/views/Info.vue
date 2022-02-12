4b488ba0e71461e49d8825a13937f6a4dc3d48dc
rawsodium <deannat2003@gmail.com>
2022-02-12 05:33:10 -0500

    color for info text

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="info">
-    <h1> Info </h1>
+    <h1 class="strokeme"> Info </h1>
     <p> You are given your first goose Huey by Mrs. Fens!</p>
     <p> Hooray!</p>
     <p> She wants you to take care of Huey. </p>
