7f03f4150ab2c5d3507236d54f2af43ab1b74449
Jcyberinc <varga.j@northeastern.edu>
2022-02-13 02:51:14 -0500

    About Changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,11 +7,11 @@
     <p>Derek Kaplan</p>
     <h2 class="strokeme">Lead Artist:</h2>
     <p>Ula Bitinaitis</p>
-    <h2 class="strokeme">Lead Composer:</h2>
+    <h2 class="strokeme">Lead Composer And HTML Expert:</h2>
     <p>Deanna Turner</p>
     <h2 class="strokeme">Lead Script Writer:</h2>
     <p>Emily Xu</p>
-    <h2 class="strokeme">Code Monkeys:</h2>
+    <h2 class="strokeme">Lead Coders:</h2>
     <p>Luis Toral and Julian Varga</p>
     <h2 class="strokeme">Non-Lead Composer:</h2>
     <p>Julian Varga</p>
