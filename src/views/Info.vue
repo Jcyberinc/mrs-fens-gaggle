2f152c09aa5292a546ed28690f9070c22d0aab74
rawsodium <deannat2003@gmail.com>
2022-02-13 00:54:35 -0500

    update info.vue

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -6,7 +6,6 @@
     <p> Breed the geese you have at your disposal, as well as any who happen to wander in, by clicking on the two you wish to breed. </p>
     <p> Your goal is to impress Ms. Fens with a goose that has a combination of the best possible traits. </p>
     <p> When you're ready for Ms. Fens to rate your goose, click the "rate" button! </p>
-    <p> If you don’t take care of them well enough, they’ll run off earlier to find a better caretaker. </p>
     <p> May the best geese win :) </p>
   </div>
 </template>
